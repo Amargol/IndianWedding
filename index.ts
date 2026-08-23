2f791c3330c127e793497377dcb3035fa07112f1
amargol <amargolshreyas@gmail.com>
2026-08-22 19:41:24 -0700

    added index

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { trishaAndAkhileshWedding } from "./exampleSchemas/trishaAndAkhileshWedding";
+import type { WeddingWebsite } from "./types/WeddingSchema";
+
+export const weddingExamples: WeddingWebsite[] = [
+    trishaAndAkhileshWedding
+];
